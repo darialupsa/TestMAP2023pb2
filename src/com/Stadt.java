a07c5e262d6bc6ddd1ac273cc8e16a4036e7ca74
Daria <darialupsa@yahoo.co>
2023-01-13 16:33:02 +0200

    first

---
+++ src/com/Stadt.java
@@ -0,0 +1,9 @@
+package com;
+
+import java.awt.*;
+
+public class Stadt {
+    private String name;
+    private String land;
+    private List temperatur;
+}
---
+++ src/com/Temperaturklasse.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Temperaturklasse {
+    private float wert;
+    private String masseinheit;
+    private int monat; //(1-12)
+
+
+}
